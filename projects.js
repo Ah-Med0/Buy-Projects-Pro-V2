e832504b0de3c8b5979370223c93bf0ad85fd555
AhMed <73498761+Ah-Med0@users.noreply.github.com>
2026-05-26 00:02:13 +0100

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,353 @@
+module.exports = [
+    {
+    name:"بروجكت شراء اعلانات تلقائي",
+    url:"https://www.mediafire.com/file/09hlwigoq9zcbd4/Adds-Bot_By_Ahmed%25282.7m%2529.zip/file",
+    price:6000
+},
+  {
+    name:"بروجكت بوت بنك مطور",
+    url:"https://www.mediafire.com/file/ns8ny5q4zeuk2tb/بوت+بنك+مطور.zip/file",
+    price:4000
+},
+      {
+       name:"بروجكت تشفير اكواد مطور",
+       url:"https://www.mediafire.com/file/ticmhqb38aet46n/EncryptedBot-By_Ahmed_Amine%25282.7m%2529.zip/file",
+       price:6000
+ },
+  {
+       name:"بيع اعضاء مطور احدث نسخة موجودة في السوق حاليا",
+       url:"https://www.mediafire.com/file/l03u1uey7p3pe8l/%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25A7%25D8%25B9%25D8%25B6%25D8%25A7%25D8%25A1_%25D9%2585%25D8%25B7%25D9%2588%25D8%25B1.rar/file",
+       price:20000
+ },
+  {
+       name:"بوت عجلة الحظ مطور احدث شيئ",
+       url:"https://www.mediafire.com/file/dhatq8nhfvvb6sd/Spin-Bot_Pro%252B_By_2.7m.zip/file",
+       price:10000
+ },
+ {
+       name:"بروجكت ريدبول نادر بيع تلقائي",
+       url:"https://www.mediafire.com/file/4bx0qden1s3v1hs/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25B1%25D9%258A%25D8%25AF%25D8%25A8%25D9%2588%25D9%2584_%25D9%2586%25D8%25A7%25D8%25AF%25D8%25B1_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AA%25D9%2584%25D9%2582%25D8%25A7%25D8%25A6%25D9%258A.zip/file",
+       price:20000
+ }, 
+    {
+        name:"بروجكت سيستم شوب اسطوري",
+        url:"https://www.mediafire.com/file/2mdqw7pxfauwrsq/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25B3%25D9%258A%25D8%25B3%25D8%25AA%25D9%2585_%25D8%25B4%25D9%2588%25D8%25A8_%25D8%25A7%25D8%25B3%25D8%25B7%25D9%2588%25D8%25B1%25D9%258A.zip/file",
+        price:2000
+    },
+    {
+        name:"بروجكت نسخ سيرفرات بتوكن مع بانل",
+        url:"https://www.mediafire.com/file/eq01ayi7122d1ff/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D9%2586%25D8%25B3%25D8%25AE_%25D8%25B3%25D9%258A%25D8%25B1%25D9%2581%25D8%25B1%25D8%25A7%25D8%25AA_%25D8%25A8%25D8%25AA%25D9%2588%25D9%2583%25D9%2586_%25D9%2585%25D8%25B9_%25D8%25A8%25D8%25A7%25D9%2586%25D9%2584.zip/file",
+        price:2000
+    },
+    {
+        name:"بروجكت بيع تلفيل حسابات",
+        url:"https://www.mediafire.com/file/vn59obdjam60624/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AA%25D9%2584%25D9%2581%25D9%258A%25D9%2584_%25D8%25AD%25D8%25B3%25D8%25A7%25D8%25A8%25D8%25A7%25D8%25AA.zip/file",
+        price:5000
+    },
+    {
+        name:"بروجكت بيع تثبيت حسابات",
+        url:"https://www.mediafire.com/file/o78t1veg3r8hfcf/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AA%25D8%25AB%25D8%25A8%25D9%258A%25D8%25AA_%25D8%25AD%25D8%25B3%25D8%25A7%25D8%25A8%25D8%25A7%25D8%25AA.zip/file",
+        price:2000
+    },
+    {
+        name:"بروجكت بيع حالة playing",
+        url:"https://www.mediafire.com/file/nm0i4rlfyqgcopo/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AD%25D8%25A7%25D9%2584%25D8%25A9_playing.zip/file",
+        price:1500
+    },
+    {
+        name:"بروجكت بيع حالة اونلاين",
+        url:"https://www.mediafire.com/file/pmuaqrib9t2iouh/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AD%25D8%25A7%25D9%2584%25D8%25A9_%25D8%25A7%25D9%2588%25D9%2586%25D9%2584%25D8%25A7%25D9%258A%25D9%2586.zip/file",
+        price:1500
+   },
+   {
+        name:"بروجكت بيع بنرات تلقائي",
+        url:"https://www.mediafire.com/file/tjthn9fdvii32oa/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25A8%25D9%2586%25D8%25B1%25D8%25A7%25D8%25AA_%25D8%25AA%25D9%2584%25D9%2582%25D8%25A7%25D8%25A6%25D9%258A.zip/file",
+        price:7000
+   },
+   {
+        name:"بروجكت بيع تلقائي ",
+        url:"https://www.mediafire.com/file/mo9fh79s0fuyg1g/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AA%25D9%2584%25D9%2582%25D8%25A7%25D8%25A6%25D9%258A_%25D9%2585%25D9%2586_%25D8%25B4%25D8%25BA%25D9%2584%25D9%2586%25D8%25A7.zip/file",
+        price:5000
+  },
+  {
+        name:"بروجكت بيع شاره المطور",
+        url:"https://www.mediafire.com/file/8z8jclm3l3pmzmd/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25B4%25D8%25A7%25D8%25B1%25D9%2587_%25D8%25A7%25D9%2584%25D9%2585%25D8%25B7%25D9%2588%25D8%25B1.zip/file",
+        price:5000
+  },
+  {
+
+        name:"بروجكت ابتايم قلتش مشفر",
+        url:"https://www.mediafire.com/file/8onn8yay64gxvl8/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A7%25D8%25A8%25D8%25AA%25D8%25A7%25D9%258A%25D9%2585_%25D9%2582%25D9%2584%25D8%25AA%25D8%25B4_%25D9%2585%25D8%25B4%25D9%2581%25D8%25B1.zip/file",
+        price:1500
+ },
+ {
+        name:"بروجكت بيع مونجو ديبي",
+        url:"https://www.mediafire.com/file/jds9leob3iz1vqg/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D9%2585%25D9%2588%25D9%2586%25D8%25AC%25D9%2588_%25D8%25AF%25D9%258A%25D8%25A8%25D9%258A.zip/file",
+       price:5000
+ },
+ {
+       name:"بروجكت بيع تشفير اكواد عادي",
+       url:"https://www.mediafire.com/file/vrtonoef8iyzfhv/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AA%25D8%25B4%25D9%2581%25D9%258A%25D8%25B1_%25D8%25A7%25D9%2583%25D9%2588%25D8%25A7%25D8%25AF_%25D8%25B9%25D8%25A7%25D8%25AF%25D9%258A.tar.zip/file",
+       price:8000
+ },
+ {
+      name:"بروجكت بيع تلفيل واونلاين واوتو",
+      url:"https://www.mediafire.com/file/6t8ueyonqpz6gol/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AA%25D9%2584%25D9%2581%25D9%258A%25D9%2584_%25D9%2588%25D8%25A7%25D9%2588%25D9%2586%25D9%2584%25D8%25A7%25D9%258A%25D9%2586_%25D9%2588%25D8%25A7%25D9%2588%25D8%25AA%25D9%2588.zip/filehttps://cdn.discordapp.com/attachments/1239696027867418695/1240220757800517773/buy_online-aoto_reaction-leveling.zip?ex=66617496&is=66602316&hm=d88ab393e08bcdd9dd8b6ce571def670e83ef937167344d93fe49b98776cef99&",
+      price:8000
+ },
+ {
+      name:"بروجكت ذكاء اصطناعي انت تحط الداتا",
+      url:"https://www.mediafire.com/file/3hlk74w68vcnunq/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25B0%25D9%2583%25D8%25A7%25D8%25A1_%25D8%25A7%25D8%25B5%25D8%25B7%25D9%2586%25D8%25A7%25D8%25B9%25D9%258A_%25D8%25A7%25D9%2586%25D8%25AA_%25D8%25AA%25D8%25AD%25D8%25B7_%25D8%25A7%25D9%2584%25D8%25AF%25D8%25A7%25D8%25AA%25D8%25A7.zip/file",
+      price:6000
+ },
+ {
+      name:"بروجكت عروض مطور",
+      url:"https://www.mediafire.com/file/3bnuhw8j04kslg4/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25B9%25D8%25B1%25D9%2588%25D8%25B6_%25D9%2585%25D8%25B7%25D9%2588%25D8%25B1_V5.zip/file",
+      price:5000
+ },
+ {
+      name:"بروجكت بيع بروجكتات سلاش",
+      url:"https://www.mediafire.com/file/uc5sia4pe70aywz/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA%25D8%25A7%25D8%25AA_%25D8%25B3%25D9%2584%25D8%25A7%25D8%25B4.zip/file",
+      price:4000
+ },
+ {
+      name:"بروجكت بيع توكنات بوتات",
+      url:"https://www.mediafire.com/file/50fr1ehlbs0ykk9/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AA%25D9%2588%25D9%2583%25D9%2586%25D8%25A7%25D8%25AA_%25D8%25A8%25D9%2588%25D8%25AA%25D8%25A7%25D8%25AA.zip/file",
+      price:3000
+ },
+ {
+      name:"بروجكت برودكاست تحكم مطور",
+      url:"https://www.mediafire.com/file/jvboha3l4t31q75/Broadcast_Controle_By_Ahmed%25282.7m%2529.zip/file",
+      price:6000
+ },  
+ {
+      name:"بروجكت فاتوره",
+      url:"https://www.mediafire.com/file/17olx9s1tormh91/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D9%2581%25D8%25A7%25D8%25AA%25D9%2588%25D8%25B1%25D9%2587.zip/file",
+      price:1500
+ },
+ {
+      name:"بروجكت تحميل مقاطع انستا وتيك",
+      url:"https://www.mediafire.com/file/y8kpisimhnnhytl/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25AA%25D8%25AD%25D9%2585%25D9%258A%25D9%2584_%25D9%2585%25D9%2582%25D8%25A7%25D8%25B7%25D8%25B9_%25D8%25A7%25D9%2586%25D8%25B3%25D8%25AA%25D8%25A7_%25D9%2588%25D8%25AA%25D9%258A%25D9%2583.zip/file",
+      price:4000
+ },
+ {
+      name:"بروجكت ميكر مطور مع ابتايم ",
+      url:"https://www.mediafire.com/file/lbc1diroyyep0lj/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D9%2585%25D9%258A%25D9%2583%25D8%25B1_%25D9%2585%25D8%25B7%25D9%2588%25D8%25B1_%25D9%2585%25D8%25B9_%25D8%25A7%25D8%25A8%25D8%25AA%25D8%25A7%25D9%258A%25D9%2585.zip/file",
+      price:6000
+ },
+ {
+      name:"بروجكت سيستم كميونتي 160 امر",
+      url:"https://www.mediafire.com/file/m9z1aoblx0gjpdy/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25B3%25D9%258A%25D8%25B3%25D8%25AA%25D9%2585_%25D9%2583%25D9%2585%25D9%258A%25D9%2588%25D9%2586%25D8%25AA%25D9%258A_160_%25D8%25A7%25D9%2585%25D8%25B1.zip/file",
+      price:6000
+ }, 
+ {
+      name:"بروجكت بيع نسخ سيرفرات بكوينز مع اشتراكات",
+      url:"https://www.mediafire.com/file/5taod23zoa78fzy/بروجكت+بيع+نسخ+سيرفرات+بكوينز+مع+اشتراكات.zip/file                                    وقم بوضع هذا الملف بدلاً من ملف subs: https://cdn.discordapp.com/attachments/864063815402389514/1400156240100987052/Subs.js?ex=688b9c9d&is=688a4b1d&hm=cf159801a261b617d12bef92475c90051be971cc43a12bd48ca67ef735d29eeb&",
+      price:9000
+ },
+    
+ {
+      name:"بروجكت نسخ سيرفرات بدون بانل",
+      url:"https://replit.com/@ce707268e0/cloner",
+      price:1500
+ },
+  
+ {
+      name:"بروجكت ردود تلقائيه",
+      url:"https://replit.com/@ahmeddarkgg158/auto-reply-rdwd-tlqyy?v=1",
+      price:500
+ },
+  
+ {
+      name:"بروجكت بيع تلفيل صوتي وكتابي",
+      url:"https://www.mediafire.com/file/arysagk5xlnttpf/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25A8%25D9%258A%25D8%25B9_%25D8%25AA%25D9%2584%25D9%2581%25D9%258A%25D9%2584_%25D8%25B5%25D9%2588%25D8%25AA%25D9%258A_%25D9%2588%25D9%2583%25D8%25AA%25D8%25A7%25D8%25A8%25D9%258A.rar/file",
+      price:10000
+ },
+    
+ {
+      name:"بروجكت طلبات",
+      url:"https://replit.com/@kinghsug/Order-Bot?v=1",
+      price:1500
+ },
+    
+ {
+      name:"بروجكت قيف اواي برفكس",
+      url:"https://replit.com/@OnlyMahmouud/Giveaways",
+      price:2500
+ },
+    
+ {
+      name:"بروجكت استلام تكت",
+      url:"https://replit.com/@Motaz-NN/Claim-Tickets-By-EZO?v=1",
+      price:8000
+ },
+    
+ {
+      name:"بروجكت قيف اواي سلاش",
+      url:"https://replit.com/@Ch2mpion/Giveaways-System-Slashs",
+      price:3000
+ },
+    
+ {
+      name:"بروجكت بيع رومات خاصه",
+      url:"https://replit.com/@OnlyMahmouud/Buy-Channels",
+      price:8000
+ },
+    
+ {
+      name:"بروجكت بيع رتب",
+      url:"https://replit.com/@OnlyMahmouud/Roles-Bot",
+      price:1500
+ },
+    
+ {
+      name:"بروجكت لوقات",
+      url:"https://replit.com/@OnlyMahmouud/Logs-Bot",
+      price:3000
+ },
+    
+ {
+      name:"بروجكت اقتراحات",
+      url:"https://replit.com/@OnlyMahmouud/Suggestions-Botv2",
+      price:2000
+ },
+    
+ {
+      name:"بروجكت حالة ستريمنق للبوتات",
+      url:"https://replit.com/@OnlyAhmed1291/Streaming-status",
+      price:3000
+ },
+    
+ {
+      name:"بروجكت منشن = ميوت",
+      url:"https://www.mediafire.com/file/33015rejzu6eu7l/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D9%2585%25D9%2586%25D8%25B4%25D9%2586_%253D_%25D9%2585%25D9%258A%25D9%2588%25D8%25AA.zip/file",
+      price:3000
+ },
+    
+ {
+      name:"بروجكت قوانين ازرار",
+      url:"https://www.mediafire.com/file/ntayllrks0uz16x/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D9%2582%25D9%2588%25D8%25A7%25D9%2586%25D9%258A%25D9%2586_%25D8%25A7%25D8%25B2%25D8%25B1%25D8%25A7%25D8%25B1.zip/file",
+      price:6000
+ },
+    
+ {
+      name:"بروجكت قوانين سيليكت مينيو",
+      url:"https://replit.com/@Policehshsa/Info-bot",
+      price:7000
+ },
+    
+ {
+      name:"بروجكت نشر اكواد",
+      url:"https://replit.com/@Iaijd7asaneii/Share-Codes",
+      price:6000
+ },
+    
+ {
+      name:"بروجكت تقديم اداره",
+      url:"https://www.mediafire.com/file/kgd5tbwi4quzlkr/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25AA%25D9%2582%25D8%25AF%25D9%258A%25D9%2585_%25D8%25A7%25D8%25AF%25D8%25A7%25D8%25B1%25D9%2587.zip/file",
+      price:3000
+ },
+    
+ {
+      name:"بروجكت ضريبه",
+      url:"https://replit.com/@Iaijd7asaneii/Tax-probot-red-Developer-Coding?v=1",
+      price:6000
+ },
+    
+ {
+      name:"بروجكت فحص روابط",
+      url:"https://replit.com/@nothinggmailcom/Check-Urls-Developer-Coding?v=1",
+      price:9000
+ },
+    
+ {
+      name:"بروجكت ترحيب",
+      url:"https://replit.com/@turkishturkish/BoldMotionlessSet",
+      price:5000
+ },
+    
+ {
+      name:"بروجكت اذكار",
+      url:"https://replit.com/@Shuruhatik/mushaf-bot-discord?v=1",
+      price:500
+ },
+    
+ {
+      name:"بروجكت قران",
+      url:"https://replit.com/@ynllaamry/bwt-qrn-krym",
+      price:500
+ },
+    
+ {
+      name:"بروجكت مسابقه صور",
+      url:"https://replit.com/@OnlyMahmouud/bwt-msbqh-Swr",
+      price:12000
+ },
+    
+ {
+      name:"بروجكت حمايه",
+      url:"https://replit.com/@OnlyMahmouud/Powerful-Protection?v=1",
+      price:7000
+ },
+    
+ {
+      name:"بروجكت تكت مطور",
+      url:"https://www.mediafire.com/file/lochgtm9vco7h4q/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25AA%25D9%2583%25D8%25AA_%25D9%2585%25D8%25B7%25D9%2588%25D8%25B1.tar.gz/file",
+      price:15000
+ },    
+ {
+      name:"بروجكت نداء",
+      url:"https://replit.com/@NotNawaf/comebot",
+      price:4000
+ },
+ {
+      name:"بروجكت نقاط اداره",
+      url:"https://replit.com/@leagimor/nqT-drh",
+      price:3500
+ },
+ {
+      name:"بروجكت خط تلقائي",
+      url:"https://www.mediafire.com/file/o8ebj7llmz98gvc/%25D8%25A8%25D8%25B1%25D9%2588%25D8%25AC%25D9%2583%25D8%25AA_%25D8%25AE%25D8%25B7_%25D8%25AA%25D9%2584%25D9%2582%25D8%25A7%25D8%25A6%25D9%258A.zip/file",
+      price:1500
+ },
+ {
+      name:"بروجكت زاجل",
+      url:"https://replit.com/@NotYazan/Majhool",
+      price:7500
+ },
+ {
+      name:"بروجكت تشهير نصابين",
+      url:"https://replit.com/@bn-lnSryhlnSry1/Mojtaba-scam-bot?v=1",
+      price:4000
+ },    
+ {
+      name:"بروجكت تشفير كلمات",
+      url:"https://replit.com/@oolgmp/v13-3#index.js",
+      price:6000
+ },
+ {
+      name:"بروجكت لفلات",
+      url:"https://www.mediafire.com/file/pty5bzlft3ub5hd/%25D9%2584%25D9%2581%25D9%2584%25D8%25A7%25D8%25AA_V13.zip/file",
+      price:5000
+},
+{
+      name:"بروجكت تحذيرات بائعين",
+      url:"https://www.mediafire.com/file/hr9z0171inmqsks/%25D8%25AA%25D8%25AD%25D8%25B0%25D9%258A%25D8%25B1_%25D8%25A8%25D8%25A7%25D8%25A6%25D8%25B9%25D9%258A%25D9%2586.zip/file",
+     price:5000
+},
+{
+     name:"بروجكت اعطاء رولات تلقائي",
+     url:"https://www.mediafire.com/file/3jb0vd7woz7wfzu/%25D8%25A7%25D8%25B9%25D8%25B7%25D8%25A7%25D8%25A1_%25D8%25B1%25D9%2588%25D9%2584_%25D8%25AA%25D9%2584%25D9%2582%25D8%25A7%25D8%25A6%25D9%258A.zip/file",
+    price:5000
+},
+{
+    name:"بروجكت تفعيل",
+    url:"https://www.mediafire.com/file/ztgo4t0h4luvmao/%25D8%25AA%25D9%2581%25D8%25B9%25D9%258A%25D9%2584_-_Verify_Bot.zip/file",
+    price:4000
+},
+{
+    name:"بروجكت منع كلمات سيئه",
+    url:"https://www.mediafire.com/file/ci6ihcplqcfcknv/%25D9%2585%25D9%2586%25D8%25B9_%25D8%25A7%25D9%2584%25D9%2583%25D9%2584%25D9%2585%25D8%25A7%25D8%25AA_%25D8%25A7%25D9%2584%25D8%25B3%25D9%258A%25D8%25A6%25D8%25A9.zip/file",
+    price:1500
+},  
+  ]
